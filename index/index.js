743ecfb3b516b50ecfb0f07cc391e3aedc071ef4
shihpaggy <107035446+shihpaggy@users.noreply.github.com>
2022-06-07 09:33:26 -0300

    Add files via upload

---
+++ index/index.js
@@ -0,0 +1,7 @@
+//abrir menu
+const botaoMenu = document.querySelector('.cabecalho_menu')
+const menu = document.querySelector('.menu-lateral')
+
+botaoMenu.addEventListener('click',()=>{
+    menu.classList.toggle('menu-lateral--active')
+})
